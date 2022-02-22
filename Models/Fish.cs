9472a4ee1b05445739a20e8e6f46fb6b691756eb
jlanglois4 <jlanglois4@my.wctc.edu>
2022-02-21 20:47:48 -0600

    Added Fish class

---
+++ Models/Fish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Fish : Animal
+    {
+        public Fish(string name)
+        {
+            Name = name;
+        }
+
+        public Fish()
+        {
+            Name = "Martin";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says glub glub");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine("Gllglglglll");
+        }
+        
+    }
+}
